a4855fecb6960b23f92c9193341c5e259f924fca
Mary1980cre <77752505+Mary1980cre@users.noreply.github.com>
2023-10-29 22:55:40 +0300

    Footer.jsx

---
+++ src/components/sections/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="navbar-expand-lg navbar-custom bg-custom">
+      <footer class="bg-custom navbar-custom">
+       
+        <div>
+          <a class="navbar-brand" href="#"><img src="./img/eqw 1.png" /></a>
+        </div>
+
+        <div>
+          <ul>
+            <li>г. Москва, Цветной б-р, 40</li>
+            <li><a class="link_footer" href="tel:+74957712111" target="_blank">+7 495 771 21 11</a></li>
+            <li><a class="link_footer" href="mailto:info@skan.ru" target="_blank">info@skan.ru</a></li>
+          </ul>
+        </div>
+
+
+    
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
