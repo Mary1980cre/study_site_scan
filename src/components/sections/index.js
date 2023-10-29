257737581bd4ff3a08ded3efae81ae977f582218
Mary1980cre <77752505+Mary1980cre@users.noreply.github.com>
2023-10-29 22:58:28 +0300

    index.js

---
+++ src/components/sections/index.js
@@ -0,0 +1,10 @@
+import '../fonts/ferry_black.woff';
+import '../fonts/Inter-Regular.woff';
+export { default as Navigation } from "../components/Navigation";
+export { default as Footer } from "../components/Footer";
+export { default as Home } from "../components/Home";
+export { default as Signin } from "../components/Signin";
+export { default as Search } from "../components/Search";
+export { default as Result } from "../components/Result";
+
+
