459671b140af7215b45dab0be14dfd325a60af72
Mary1980cre <77752505+Mary1980cre@users.noreply.github.com>
2023-10-29 22:56:19 +0300

    Home.jsx

---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Hero from "./../components/sections/Hero";
+import Whyus from "./../components/sections/Whyus";
+import Price from "./../components/sections/Price";
+
+export default function Index() {
+    return (
+        <>
+            <Hero />
+            <Whyus />
+            <Price />
+        </>
+    );
+}
