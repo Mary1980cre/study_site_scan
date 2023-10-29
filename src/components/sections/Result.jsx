fff8ddb5e9ad7929f640a5f3dfaf5294cbc5d290
Mary1980cre <77752505+Mary1980cre@users.noreply.github.com>
2023-10-29 22:59:38 +0300

    Result.jsx

---
+++ src/components/sections/Result.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+function Resoult() {
+  return (
+    <div class="container">
+
+    <div class="row align-items-center g-5 py-5">
+      
+
+      <div class="col-lg-6">"
+        <h1 class="display-5 fw-bold text-body-emphasis lh-1 mb-3">Ищем. Скоро будут результаты</h1>
+            <p class="hero-descr">Поиск может занять некоторое время, просим сохранять терпение.</p>
+        
+
+      </div>
+      
+      <div class="col-10 col-sm-8 col-lg-6">
+        <img src="../img/Group 1171274267.png" class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy" />
+      </div>
+    </div>
+    </div>
+
+
+  );
+}
+
+export default Resoult;
